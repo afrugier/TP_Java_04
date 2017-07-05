acccd01e22c39b9abab6be40394388228873d465
Anthony Frugier <fru.anthony@gmail.com>
2017-07-05 15:23:56 +0200

    Pizzeria avec Objet

---
+++ pizzeria-console-objet/src/main/java/fr/pizzeria/model/Pizza.java
@@ -0,0 +1,49 @@
+package fr.pizzeria.model;
+
+public class Pizza {
+
+	int id;
+	String code;
+	String nom;
+	double prix;
+	
+	public Pizza(int id, String code, String nom, double prix) {
+		this.id = id;
+		this.nom = nom;
+		this.code = code;
+		this.prix = prix;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public double getPrix() {
+		return prix;
+	}
+
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+
+}
